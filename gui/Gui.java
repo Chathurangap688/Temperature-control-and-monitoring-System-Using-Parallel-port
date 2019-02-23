3718c4eab83bb843b84933ea7432991a6088cd20
Thilakawansha B.M.C.P <bmcpthilakawansha@gmail.com>
2019-02-23 19:24:40 +0530

    java Gui

---
+++ gui/Gui.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import static java.lang.Thread.sleep;
+
+/**
+ *
+ * @author CHATHURANGA
+ */
+public class Gui {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws InterruptedException {
+        // TODO code application logic here
+        mainFrame frame = new mainFrame();
+        frame.gui();
+        State state = new State();
+        Server server = new Server();
+        server.start();
+        while(true){
+            state.update();
+            state.stateRefresh();
+            sleep(100);
+        }
+    }
+    
+}
